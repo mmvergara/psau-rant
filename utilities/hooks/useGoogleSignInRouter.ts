e6f2aa58a0c5842ebea11418617781cde8311ca1
mmvergara <mark.jesusmanabat@gmail.com>
2023-03-25 08:48:59 +0800

    update useGoogleSignInRouter

--- utilities/hooks/useGoogleSignInRouter.ts
+++ utilities/hooks/useGoogleSignInRouter.ts
@@ -1,12 +1,12 @@
 import { FirebaseAuth } from "@/firebase/Firebase-Client";
 import { useRouter } from "next/router";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useAuthState, useSignInWithGoogle } from "react-firebase-hooks/auth";
 
 const useGoogleSignInRouter = () => {
-  const [signInWithGoogle, user, loading, error] =
-    useSignInWithGoogle(FirebaseAuth);
+  const [signInWithGoogle] = useSignInWithGoogle(FirebaseAuth);
+  const [user, loading] = useAuthState(FirebaseAuth);
   const router = useRouter();
-  return { user, loading, error, router, signInWithGoogle };
+  return { user, router, loading, signInWithGoogle };
 };
 
 export default useGoogleSignInRouter;

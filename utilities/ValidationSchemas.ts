57c0978d54a52b8d57cb911951b8e7b756fa644f
mmvergara <mark.jesusmanabat@gmail.com>
2023-03-25 10:16:56 +0800

    refactor validation schema

--- utilities/ValidationSchemas.ts
+++ utilities/ValidationSchemas.ts
@@ -1,11 +1,27 @@
 import { object, string } from "yup";
 
-export const authSchema = object({
-  email: string()
-    .email("Please enter a valid email address")
-    .required("Email field is required"),
-  password: string()
-    .min(6, "Minimun of 6 characters for the password")
-    .max(255)
-    .required("Password field is required"),
+const email = string()
+  .email("Please enter a valid email address")
+  .required("Email field is required");
+const password = string()
+  .min(6, "Minimun of 6 characters for the password")
+  .max(255)
+  .required("Password field is required");
+const username = string()
+  .min(3, "Minimun of 3 characters for the username")
+  .max(255)
+  .required("Username field is required");
+
+  
+export const signinSchema = object({
+  email,
+  password,
+});
+export const signupSchema = object({
+  username,
+  email,
+  password,
+});
+export const updateUsernameSchema = object({
+  username,
 });

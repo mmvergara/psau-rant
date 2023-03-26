cb162072c318a6b68980aa6d6ba75a97ad08b46d
mmvergara <mark.jesusmanabat@gmail.com>
2023-03-26 20:55:38 +0800

    Rant Component finished

--- components/Rant/Rant.tsx
+++ components/Rant/Rant.tsx
@@ -4,11 +4,14 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { getTimeElapsedString } from "@/utilities/Date";
 import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
+import { RantWithId } from "@/types/models/rant_types";
+type Props = {
+  rantWithId: RantWithId;
+};
+const Rant = ({ rantWithId }: Props) => {
+  const { rant_title, rant_content, rant_author, rant_date } = rantWithId;
 
-const Rant = () => {
-  const timeElapsed = getTimeElapsedString(
-    new Date("Sun Mar 26 2023 07:38:18 GMT+0800 (Singapore Standard Time)")
-  );
+  const timeElapsed = getTimeElapsedString(new Date(rant_date.toDate()));
   return (
     <Box
       component="article"
@@ -28,7 +31,7 @@ const Rant = () => {
           borderRadius: "5px 5px 0px 0px",
         }}
       >
-        <Typography variant="h5">Rant Title</Typography>
+        <Typography variant="h5">{rant_title}</Typography>
       </Box>
       <Box
         sx={{
@@ -38,11 +41,7 @@ const Rant = () => {
           borderRadius: "0px 0px 5px 5px",
         }}
       >
-        <Typography variant="body1">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-          voluptates, quod, quia, voluptate quae quibusdam voluptatem quos
-          voluptatum quas quidem quas quidem quas quidem quas quidem quas quidem
-        </Typography>
+        <Typography variant="body1">{rant_content}</Typography>
         <Box
           sx={{
             display: "flex",
@@ -57,7 +56,7 @@ const Rant = () => {
             color="dimgray"
             sx={{ display: "flex", flexDirection: "column" }}
           >
-            <Typography sx={{ fontSize: "13px" }}>Author Name </Typography>
+            <Typography sx={{ fontSize: "13px" }}>{rant_author}</Typography>
             <Typography sx={{ display: "inline", fontSize: 10 }}>
               <Divider />
               {timeElapsed}
@@ -67,7 +66,13 @@ const Rant = () => {
           <Button
             color="error"
             variant="contained"
-            sx={{ display: "flex", gap: 1 }}
+            sx={{
+              display: "flex",
+              gap: 1,
+              borderRadius: "20px",
+              px: 0,
+              bgcolor: "#f23c3d",
+            }}
           >
             <FavoriteBorderIcon /> 2
           </Button>

b0b47073b9ec1c52eae4c66ab19bca1ff597a3f8
mmvergara <mark.jesusmanabat@gmail.com>
2023-04-01 07:25:55 +0800

    Added flexGrow on Loading Circlce

--- components/Layout/CenterCircularProgress.tsx
+++ components/Layout/CenterCircularProgress.tsx
@@ -3,7 +3,7 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 const CenterCircularProgress: React.FC = () => {
   return (
-    <Box sx={{ display: "flex", justifyContent: "center", mt: "10vh" }}>
+    <Box sx={{ display: "flex", justifyContent: "center", mt: "10vh" ,flexGrow:1}}>
       <CircularProgress />
     </Box>
   );

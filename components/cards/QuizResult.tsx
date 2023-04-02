ec48f32a990b5398690aa4d508ad54debcbd24ac
mmvergara <mark.jesusmanabat@gmail.com>
2023-04-02 10:55:08 +0800

    added shuffle switch btn

--- components/cards/QuizResult.tsx
+++ components/cards/QuizResult.tsx
@@ -1,5 +1,14 @@
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import {
+  Divider,
+  FormControlLabel,
+  FormGroup,
+  Switch,
+  Typography,
+} from "@mui/material";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 type Props = {
   shuffled: boolean;
@@ -14,15 +23,67 @@ const QuizResult = ({
   isStillLearningCardsId,
   shuffled,
 }: Props) => {
+  const router = useRouter();
+  const [isShuffled, setIsShuffled] = useState<boolean>(shuffled);
+  console.log("isShuffled", isShuffled);
   return (
-    <Box sx={{ display: "flex", flexDirection: "column" }}>
-      <Button onClick={() => onResetCards(null, shuffled)}>Replay Cards</Button>
-      <Button onClick={() => onResetCards(isStillLearningCardsId, true)}>
-        Replay "Still Learning" Cards
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        bgcolor: "white",
+        p: 2,
+        borderTop: "10px solid green",
+        width: "100%",
+        maxWidth: "500px",
+      }}
+    >
+      <Typography>Result</Typography>
+      <Typography>
+        {isStillLearningCardsId.length} "Still Learning" Cards.
+      </Typography>
+      <Typography>{iKnowCardsId.length} "I Know" Cards</Typography>
+      <Button variant="outlined" onClick={() => router.push("/cards")}>
+        Go Back to My Cards
       </Button>
-      <Button onClick={() => onResetCards(iKnowCardsId, true)}>
-        Replay "I Know" Cards
+      <Divider sx={{ my: 2 }} />
+      <Typography>Take a quiz again!</Typography>
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch
+              defaultChecked={shuffled}
+              onChange={(e) => setIsShuffled(e.target.checked)}
+            />
+          }
+          label="Shuffled"
+        />
+      </FormGroup>
+      {isStillLearningCardsId.length !== 0 && (
+        <Button
+          sx={{ my: 1 }}
+          variant="contained"
+          onClick={() => onResetCards(isStillLearningCardsId, isShuffled)}
+        >
+          "Still Learning" Cards
+        </Button>
+      )}
+      <Button
+        sx={{ my: 1 }}
+        variant="contained"
+        onClick={() => onResetCards(iKnowCardsId, isShuffled)}
+      >
+        All Cards
       </Button>
+      {iKnowCardsId.length !== 0 && (
+        <Button
+          sx={{ my: 1 }}
+          variant="outlined"
+          onClick={() => onResetCards(iKnowCardsId, isShuffled)}
+        >
+          "I Know" Cards
+        </Button>
+      )}
     </Box>
   );
 };

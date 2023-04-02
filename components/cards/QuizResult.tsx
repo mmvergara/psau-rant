a3ad9f0da500926faa343304e2a9f4aef81bc95b
mmvergara <mark.jesusmanabat@gmail.com>
2023-04-02 09:29:53 +0800

    update

---
+++ components/cards/QuizResult.tsx
@@ -0,0 +1,30 @@
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+
+type Props = {
+  shuffled: boolean;
+  isStillLearningCardsId: string[];
+  iKnowCardsId: string[];
+  onResetCards: (id: string[] | null, shuffle: boolean) => void;
+};
+
+const QuizResult = ({
+  onResetCards,
+  iKnowCardsId,
+  isStillLearningCardsId,
+  shuffled,
+}: Props) => {
+  return (
+    <Box>
+      <Button onClick={() => onResetCards(null, shuffled)}>Replay Cards</Button>
+      <Button onClick={() => onResetCards(isStillLearningCardsId, true)}>
+        Replay "Still Learning" Cards
+      </Button>
+      <Button onClick={() => onResetCards(iKnowCardsId, true)}>
+        Replay "I Know" Cards
+      </Button>
+    </Box>
+  );
+};
+
+export default QuizResult;
--- pages/cards/[cardset]/quiz.tsx
+++ pages/cards/[cardset]/quiz.tsx
@@ -4,54 +4,76 @@ import CenterCircularProgress from "@/components/Layout/CenterCircularProgress";
 import Container from "@mui/material/Container";
 import Card from "@/components/cards/Card";
 import Box from "@mui/material/Box";
-import Fab from "@mui/material/Fab";
 import { useEffect, useState } from "react";
 import { CardExamConfig } from "@/types/models/card_types";
 import { getCardSetById } from "@/firebase/services/cards_services";
 import { Typography } from "@mui/material";
 import { useRouter } from "next/router";
 import { toast } from "react-toastify";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import useKeyPress from "@/utilities/hooks/useKeyPress";
 
 const CardSetExamPage: React.FC = () => {
   const router = useRouter();
-  const cardset = router.query.cardset as string;
+  const isMobile = useMediaQuery("(max-width: 600px");
+
+  const cardsetid = router.query.cardset as string;
   const termFirst = !!router.query.termFirst;
   const shuffled = !!router.query.shuffled;
 
   const [isFetching, setIsFetching] = useState<boolean>(true);
-  const [cardData, setCardData] = useState<Card[]>([]);
+
+  const [cardSet, setCardSet] = useState<Card[]>([]);
   const [config, setConfig] = useState<CardExamConfig>({
     termFirst,
     activeCardId: "1",
     actionDirection: "previous",
   });
 
-  const showNextCard = () => {
-    const nextCardId = cardData.findIndex(
+  const showNextCard = (quiz: "StillLearning" | "Know") => {
+    const nextCardId = cardSet.findIndex(
       (card) => card.card_id === config.activeCardId
     );
-    if (nextCardId + 1 >= cardData.length) return;
+
+    if (quiz === "Know") {
+      setIKnowCardsId([...iKnowCardsId, config.activeCardId]);
+    }
+    if (quiz === "StillLearning") {
+      setIsStillLearningCardsId([
+        ...isStillLearningCardsId,
+        config.activeCardId,
+      ]);
+    }
+
+    // if it's the last card, show the result
+    if (nextCardId + 1 >= cardSet.length) {
+      setEnded
+      return;
+    }
+
     setConfig({
       ...config,
-      activeCardId: cardData[nextCardId + 1].card_id,
+      activeCardId: cardSet[nextCardId + 1].card_id,
       actionDirection: "next",
     });
   };
 
   const showPreviousCard = () => {
-    const nextCardId = cardData.findIndex(
+    const nextCardId = cardSet.findIndex(
       (card) => card.card_id === config.activeCardId
     );
     if (nextCardId - 1 < 0) return;
     setConfig({
       ...config,
-      activeCardId: cardData[nextCardId - 1].card_id,
+      activeCardId: cardSet[nextCardId - 1].card_id,
       actionDirection: "previous",
     });
   };
 
   const getCardSet = async () => {
-    const { data, error } = await getCardSetById(cardset as string);
+    const { data, error } = await getCardSetById(cardsetid as string);
     setIsFetching(false);
     if (error) return toast.error(error);
     if (data) {
@@ -64,9 +86,46 @@ const CardSetExamPage: React.FC = () => {
         }));
         cardSet.sort((a, b) => +a.card_id - +b.card_id);
       }
-      setCardData(cardSet);
+      setCardSet(cardSet);
     }
   };
+
+  // Card Reset Functions
+  const [ended, setEnded] = useState<boolean>(false);
+  const [iKnowCardsId, setIKnowCardsId] = useState<string[]>([]);
+  const [isStillLearningCardsId, setIsStillLearningCardsId] = useState<
+    string[]
+  >([]);
+  const handleResetCards = (
+    filterdCards: string[] | null,
+    shuffled: boolean
+  ) => {
+    setIsFetching(true);
+    let newCardSet = cardSet;
+    if (filterdCards) {
+      console.log("filterdCards", filterdCards);
+      newCardSet = newCardSet.filter((card) =>
+        filterdCards.includes(card.card_id)
+      );
+    }
+    if (shuffled) {
+      newCardSet.sort(() => Math.random() - 0.5);
+      newCardSet = newCardSet.map((card, index) => ({
+        ...card,
+        card_id: String(index + 1),
+      }));
+      newCardSet.sort((a, b) => +a.card_id - +b.card_id);
+    }
+
+    setCardSet(newCardSet);
+    setConfig({
+      ...config,
+      activeCardId: "1",
+      actionDirection: "previous",
+    });
+    setIsFetching(false);
+  };
+  // Card Reset Functions
 
   useEffect(() => {
     getCardSet();
@@ -76,13 +135,13 @@ const CardSetExamPage: React.FC = () => {
   const rightArrow = useKeyPress("ArrowRight");
   const leftArrow = useKeyPress("ArrowLeft");
   useEffect(() => {
-    if (rightArrow) showNextCard();
+    if (rightArrow) showNextCard("StillLearning");
     if (leftArrow) showPreviousCard();
   }, [rightArrow, leftArrow]);
 
   // Render
   if (isFetching) return <CenterCircularProgress />;
-  if (!isFetching && cardData.length === 0) {
+  if (!isFetching && cardSet.length === 0) {
     toast.error("Card set not found");
     router.push("/");
   }
@@ -106,75 +165,45 @@ const CardSetExamPage: React.FC = () => {
           textAlign: "center",
           mb: 2,
         }}
-      >{`${config.activeCardId} / ${cardData.length}`}</Typography>
-      {cardData.map((card) => {
+      >{`${config.activeCardId} / ${cardSet.length}`}</Typography>
+      {cardSet.map((card) => {
         return <Card cardData={card} config={config} />;
       })}
       <Box
         sx={{
           display: "flex",
-          justifyContent: "center",
+          justifyContent: "space-around",
           alignItems: "center",
-          width: "100%",
-          gap: 3,
+          width: isMobile ? "100%" : "60%",
           mt: 2,
         }}
       >
-        <Fab
-          variant="extended"
-          color="primary"
-          aria-label="add"
-          sx={{ width: "150px" }}
-          onClick={showPreviousCard}
-          disabled={config.activeCardId === "1"}
-        >
-          <KeyboardArrowLeftIcon />
-        </Fab>
-
-        <Fab
-          variant="extended"
-          color="primary"
-          aria-label="add"
-          sx={{ width: "150px" }}
-          onClick={showNextCard}
-          disabled={
-            config.activeCardId === cardData[cardData.length - 1].card_id
-          }
-        >
-          <KeyboardArrowRightIcon />
-        </Fab>
+        <ButtonGroup disableElevation variant="contained">
+          <Button
+            sx={{ py: 2, borderRadius: 8 }}
+            onClick={() => showPreviousCard()}
+          >
+            <KeyboardArrowLeftIcon sx={{ mr: isMobile ? 0 : 0.5 }} />
+            {!isMobile && "Back"}
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup disableElevation variant="contained">
+          <Button
+            sx={{ py: 2, borderRadius: 10 }}
+            onClick={() => showNextCard("StillLearning")}
+          >
+            Still Learning
+          </Button>
+          <Button
+            sx={{ py: 2, borderRadius: 10 }}
+            onClick={() => showNextCard("Know")}
+          >
+            I know <KeyboardArrowRightIcon />
+          </Button>
+        </ButtonGroup>
       </Box>
     </Container>
   );
 };
 
 export default CardSetExamPage;
-
-// Hook
-function useKeyPress(targetKey: string) {
-  // State for keeping track of whether key is pressed
-  const [keyPressed, setKeyPressed] = useState<boolean>(false);
-  // If pressed key is our target key then set to true
-  function downHandler({ key }: KeyboardEvent) {
-    if (key === targetKey) {
-      setKeyPressed(true);
-    }
-  }
-  function upHandler({ key }: KeyboardEvent) {
-    if (key === targetKey) {
-      setKeyPressed(false);
-    }
-  }
-
-  // Add event listeners
-  useEffect(() => {
-    window.addEventListener("keydown", downHandler);
-    window.addEventListener("keyup", upHandler);
-    // Remove event listeners on cleanup
-    return () => {
-      window.removeEventListener("keydown", upHandler);
-      window.removeEventListener("keydown", downHandler);
-    };
-  }, []); // Empty array ensures that effect is only run on mount and unmount
-  return keyPressed;
-}

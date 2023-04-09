e99bd5131fa04f03aeb84c6e4a4cf4efef0ffd7e
mmvergara <mark.jesusmanabat@gmail.com>
2023-04-09 20:21:28 +0800

    refactor Question component

--- components/cards/Quiz/Question.tsx
+++ components/cards/Quiz/Question.tsx
@@ -1,48 +1,86 @@
-import { CardQuiz } from "@/types/models/card_types";
-import {
-  Box,
-  Divider,
-  FormControl,
-  FormLabel,
-  Paper,
-  Typography,
-} from "@mui/material";
+import { AnsweredResults, CardQuiz } from "@/types/models/card_types";
+import { Divider, Paper, Typography, Alert } from "@mui/material";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 
 type Props = {
+  index: number;
+  isSubmitted: boolean;
+  onAnswer: (result: AnsweredResults) => void;
   cardQuiz: CardQuiz;
   choicesType: "term" | "definition";
-  index: number;
+  answeredQuestions: AnsweredResults[];
 };
-const Question = ({ cardQuiz, choicesType, index }: Props) => {
+const Question = ({
+  index,
+  isSubmitted,
+  onAnswer,
+  cardQuiz,
+  choicesType,
+  answeredQuestions,
+}: Props) => {
   const question =
     cardQuiz[`card_${choicesType === "term" ? "definition" : "term"}`];
+  const answer = cardQuiz.card_choices[cardQuiz.card_correct_choice];
+  const AnsweredResults: AnsweredResults | null =
+    answeredQuestions.find((v) => v.card_id === cardQuiz.card_id) || null;
 
   const choices = Object.entries(cardQuiz.card_choices);
-  console.log(choices);
+
+  const isCorrect = isSubmitted && AnsweredResults && AnsweredResults.isCorrect;
+  console.log({ isSubmitted });
+  const borderColor = !isSubmitted
+    ? "gray"
+    : isSubmitted && !AnsweredResults
+    ? "teal"
+    : isCorrect
+    ? "green"
+    : "red";
+
+  const correctAnswer =
+    (isSubmitted && AnsweredResults && AnsweredResults.correct_answer) || null;
+  console.log("rerender");
   return (
-    <Paper sx={{ bgcolor: "white", p: 2, boxShadow: 4 }}>
+    <Paper
+      sx={{
+        bgcolor: "white",
+        p: 2,
+        boxShadow: 4,
+        border: `4px solid ${borderColor}`,
+      }}
+    >
       <Typography>
         {index + 1}. {question}
       </Typography>
       <Divider sx={{ width: "100%", my: 2 }} />
       <RadioGroup
-        aria-labelledby="demo-radio-buttons-group-label"
-        defaultValue="female"
-        name="radio-buttons-group"
+        onChange={(e) => {
+          onAnswer({
+            card_id: cardQuiz.card_id,
+            isCorrect: e.target.value === answer,
+            answer: e.target.value,
+            correct_answer: answer,
+          });
+        }}
       >
         {choices.map(([letter, choice], index) => {
           return (
             <FormControlLabel
-              value={letter}
+              value={choice}
               control={<Radio />}
-              label={`${choice}`}
+              label={choice}
             />
           );
         })}
       </RadioGroup>
+      {correctAnswer && AnsweredResults && (
+        <Alert severity={AnsweredResults.isCorrect ? "success" : "error"}>
+          {AnsweredResults.isCorrect
+            ? "Correct!"
+            : `Correct answer is: ${correctAnswer}`}
+        </Alert>
+      )}
     </Paper>
   );
 };

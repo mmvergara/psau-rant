1151631509112b915e63c35b077b79494195431b
mmvergara <mark.jesusmanabat@gmail.com>
2023-04-10 18:08:36 +0800

    add preview page

---
+++ pages/cards/[cardsetid]/preview.tsx
@@ -0,0 +1,44 @@
+import CardPreviewContent from "@/components/Cards/CardPreviewBox";
+import CenterCircularProgress from "@/components/Layout/CenterCircularProgress";
+import { getCardSetById } from "@/firebase/services/cards_services";
+import { CardSet } from "@/types/models/card_types";
+import { Container } from "@mui/material";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+const CardPreviewPage = () => {
+  const router = useRouter();
+  const handleDeleteCard = () => router.push("/cards");
+  const cardsetid = router.query.cardsetid as string;
+  const [cardSet, setCardSet] = useState<CardSet | null>(null);
+  const fetchCardSet = async () => {
+    const { data, error } = await getCardSetById(cardsetid);
+    if (error) {
+      toast.error(error);
+      router.push("/cards");
+    }
+    if (data) return setCardSet(data);
+    router.push("/cards");
+  };
+  useEffect(() => {
+    fetchCardSet();
+  }, []);
+
+  return (
+    <>
+      {cardSet ? (
+        <Container maxWidth="sm" sx={{ mt: 4 }}>
+          <CardPreviewContent
+            cardSet={cardSet}
+            onCardDelete={handleDeleteCard}
+          />
+        </Container>
+      ) : (
+        <CenterCircularProgress />
+      )}
+    </>
+  );
+};
+
+export default CardPreviewPage;

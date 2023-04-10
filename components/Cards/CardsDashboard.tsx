29d63c82a1f4c9486fbeb7b16913a75b6be5b4b6
mmvergara <mark.jesusmanabat@gmail.com>
2023-04-10 10:26:08 +0800

    update

---
+++ components/Cards/CardsDashboard.tsx
@@ -0,0 +1,51 @@
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import Link from "next/link";
+import CardsList from "./CardsLists";
+
+const CardsDashboard = () => {
+  return (
+    <Container sx={{ mt: 3 }}>
+      <Box sx={{ bgcolor: "white" }}>
+        <Box
+          sx={{
+            bgcolor: "primary.main",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            p: 1,
+          }}
+        >
+          <Typography sx={{ color: "white" }}>Flip Cards </Typography>
+        </Box>
+        <Box sx={{ p: 2 }}>
+          <Stack spacing={2}>
+            <Button
+              LinkComponent={Link}
+              href={"/cards/create"}
+              variant="contained"
+              sx={{ width: "fit-content" }}
+            >
+              Create New Card Set
+            </Button>
+            <TextField
+              label="Search Flip Cards"
+              sx={{ width: "fit-content" }}
+            />
+            <Divider />
+            <CardsList />
+          </Stack>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default CardsDashboard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import Rants from "@/components/Rant/Rants";
+import Head from "next/head";
 
 function Home() {
   return (
     <>
+      <Head>
+        <title>Home</title>
+      </Head>
       <Rants />
     </>
   );
